7d412e63416ed027b2d5d35002239d5492cc117e
aoelarsen <39989504+aoelarsen@users.noreply.github.com>
2021-10-24 23:31:02 +0200

    added some text. Added react-icons package. Added styles to anchors deleted home.css and oldGlobal.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <header className="p-5 md:p-7 lg:p-10">
-      <h1 className="font-sans font-bold text-2xl md:text-3xl">AOLar.no</h1>
-      <p className="font-serif">Anders Øksendal Larsen</p>
+      <h1 className="font-bold text-2xl md:text-3xl">AOL<span className="font-thin pl-[0.15rem]">ar.no</span></h1>
+      <p className="mt-1">Anders Øksendal Larsen</p>
     </header>
   )
 }
